1c7bb043c6fc1b744200d6a81d178030e76ae27e
LukeCali-949 <mengluke17@gmail.com>
2024-05-01 19:53:46 -0400

    hey v2

--- src/test/java/edu/iu/habahram/ducksservice/repository/DucksFileRepositoryTest.java
+++ src/test/java/edu/iu/habahram/ducksservice/repository/DucksFileRepositoryTest.java
@@ -1,24 +1,7 @@
 package edu.iu.habahram.ducksservice.repository;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class DucksFileRepositoryTest {
+class InventoryRepositoryTest {
 
-    @Test
-    void updateImage() {
-    }
-
-    @Test
-    void updateAudio() {
-    }
-
-    @Test
-    void getImage() {
-    }
-
-    @Test
-    void getAudio() {
-    }
 }
