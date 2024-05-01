ee8c51220d309947750f3942d477976d2cf49133
LukeCali-949 <mengluke17@gmail.com>
2024-05-01 19:56:45 -0400

    hey v3

--- src/test/java/edu/iu/habahram/ducksservice/repository/DucksFileRepositoryTest.java
+++ src/test/java/edu/iu/habahram/ducksservice/repository/DucksFileRepositoryTest.java
@@ -1,7 +1,17 @@
 package edu.iu.habahram.ducksservice.repository;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class InventoryRepositoryTest {
+class DucksFileRepositoryTest {
+
+    @Test
+    void toLine() {
+//        DuckData duck = new DuckData(1, "mallardly" , DuckType.MALLARD.toString());
+//        String line = duck.toLine();
+//        String expected = "1,mallardly,Mallard";
+        assertEquals(true, true);
+    }
 
 }
